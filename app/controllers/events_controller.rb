59d3e5351f054f16fffcb56f840ce298ddba8310
yourloff <alex.yourloff@gmail.com>
2022-05-15 13:39:38 +0700

    add users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,12 +58,13 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
   def event_params
     params.require(:event).permit(:title, :address, :datetime, :description)
   end
