61541caa1697219844f2ff78b1dbf9e52f160f6b
yourloff <alex.yourloff@gmail.com>
2022-05-19 09:35:10 +0700

    fix alerts, add bootstrap_flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, length: { maximum: 255 }
   validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   before_validation :set_name, on: :create
 
