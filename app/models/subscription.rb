4d7f56e038522d5baaed7f912b43454cc6fee6bf
yourloff <alex.yourloff@gmail.com>
2022-05-21 16:10:59 +0700

    fix links comments&subscriptions

---
+++ app/models/subscription.rb
@@ -0,0 +1,19 @@
+class Subscription < ApplicationRecord
+  belongs_to :event
+  belongs_to :user
+
+  validates :event, presence: true
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+
+  validates :user, uniqueness: { scope: :event_id, if: -> { user.present? } }
+  validates :user_email, uniqueness: { scope: :event_id, unless: -> { user.present? } }
+
+  def user_name
+    user&.name || super
+  end
+
+  def user_email
+    user&.email || super
+  end
+end
