924f9d95fe3c60e52b6bf4b652da18ea0b9928ed
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-18 10:52:22 +0700

    fix map.js

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,31 +1,37 @@
-ymaps.ready(init);
+document.addEventListener('turbo:load', () => {
+    ymaps.ready(init);
+    var myMap;
 
-function init() {
-    address = document.getElementById('map').getAttribute('data-address');
+    function init() {
+        if (!document.getElementById('map'))
+            return
 
-    myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 10
-    });
+        address = document.getElementById('map').getAttribute('data-address');
 
-    myGeocoder = ymaps.geocode(address);
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 10
+        });
 
-    myGeocoder.then(
-        function (res) {
-            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+        myGeocoder = ymaps.geocode(address);
 
-            myMap.geoObjects.add(
-                new ymaps.Placemark(
-                    coordinates,
-                    { iconContent: address },
-                    { preset: 'islands#blueStretchyIcon' }
-                )
-            );
+        myGeocoder.then(
+            function (res) {
+                coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-            myMap.setCenter(coordinates);
-            myMap.setZoom(15);
-        }, function (err) {
-            alert('Ошибка при определении местоположения');
-        }
-    );
-}
+                myMap.geoObjects.add(
+                    new ymaps.Placemark(
+                        coordinates,
+                        {iconContent: address},
+                        {preset: 'islands#blueStretchyIcon'}
+                    )
+                );
+
+                myMap.setCenter(coordinates);
+                myMap.setZoom(15);
+            }, function (err) {
+                alert('Ошибка при определении местоположения');
+            }
+        );
+    }
+})
