d9b12c8900ab79bf4abbcdfc36578032df994cb0
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-20 09:03:36 +0700

    откат изменений

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module BBQ
 
     config.i18n.default_locale = :ru
 
-    config.serve_static_assets = true
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
