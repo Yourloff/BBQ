ca8af1be67aa696e5343da4ea5228046e22e4697
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-25 12:20:02 +0700

    add mailer comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,5 +70,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'yourloff.ru' }
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :sendmail
 end
