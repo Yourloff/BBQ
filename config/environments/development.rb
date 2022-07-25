34b43d3d7573c000ea1c7eca0897ae829d36de36
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-25 12:00:06 +0700

    add mailer photos&subscriptions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,16 +68,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :tls => true,
-    address: 'smtp.yandex.ru',
-    domain: 'yandex.ru',
-    port: '465',
-    user_name: 'alex.orloff99@yandex.ru',
-    password: '',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: 'yourloff.ru' }
+
+  config.action_mailer.delivery_method = :letter_opener
 end
