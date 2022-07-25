e751b2686e37bd9a9dc3488197da44ed025ff765
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-25 15:17:53 +0700

    fix path deploy_to

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:Yourloff/BBQ.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/www/"
+set :deploy_to, "/home/deploy/www/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
