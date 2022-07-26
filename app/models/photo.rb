81cda6a73fba9461d2cb83b419aa69cb62ab6ac3
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-26 10:25:52 +0700

    fix model photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,9 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :event, presence: true
-  validates :user, presence: true
-  validates :photo, presence: true
+  validates :photo, presence: true, optional: true
 
   mount_uploader :photo, PhotoUploader
 
