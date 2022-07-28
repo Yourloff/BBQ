1e6fbe6b53f091d288baba2234e296f4ca3bf2aa
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-28 14:38:51 +0700

    fix models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :photo, presence: true
-
   mount_uploader :photo, PhotoUploader
 
   scope :persisted, -> { where "id IS NOT NULL" }
