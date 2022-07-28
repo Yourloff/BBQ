f81736289942c2a4bd9a782455f72fed7c99288f
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-28 15:18:02 +0700

    fix mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,10 +1,9 @@
 class EventMailer < ApplicationMailer
-  def subscription(subscription)
-    @email = subscription.user_email
+  def subscription(subscription, email)
     @name = subscription.user_name
     @event = subscription.event
 
-    mail to: event.user.email, subject: default_i18n_subject(event: @event.title)
+    mail to: email, subject: default_i18n_subject(event: @event.title)
   end
 
   def comment(comment, email)
