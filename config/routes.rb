d02902cf866980dad419cc5f9df72a6aa9bbcd74
Alexey Orloff <alex.yourloff@gmail.com>
2022-08-19 10:18:15 +0700

    add prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callback: 'users/omniauth_callbacks' }
   resources :photos
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show, :edit, :update]
   root "events#index"
 
