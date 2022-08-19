520a44d0a069be596fa209133644b4e9e9fe8409
Alexey Orloff <alex.yourloff@gmail.com>
2022-08-19 10:59:58 +0700

    omniauth_callbackS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callback: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :photos
   resources :users, only: [:show, :edit, :update]
   root "events#index"
