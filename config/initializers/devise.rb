ce33a900c170993b15cb5348cdf8c0cb2c66567a
Alexey Orloff <alex.yourloff@gmail.com>
2022-08-22 10:49:29 +0700

    params vk

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -278,7 +278,13 @@ Devise.setup do |config|
                   Rails.application.credentials.dig(:github, :github_client_secret), scope: 'user:email'
 
   config.omniauth :vkontakte, Rails.application.credentials.dig(:vkontakte, :vk_client_id),
-                  Rails.application.credentials.dig(:vkontakte, :vk_client_secret), { scope: 'email' }
+                  Rails.application.credentials.dig(:vkontakte, :vk_client_secret),
+                  {
+                    scope: 'email',
+                    lang: 'ru',
+                    https: 0,
+                    redirect_uri: 'http://yourloff.ru/users/auth/vkontakte/callback'
+                  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
